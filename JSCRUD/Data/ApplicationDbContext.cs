e6c47f9a65696188a04678198e750e75d63b9f77
Joe-Ortiz <7156063+Joe-Ortiz@users.noreply.github.com>
2026-08-24 13:36:26 -0700

    Add project files.

---
+++ JSCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JSCRUD.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<JSCRUD.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ JSCRUD/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JSCRUD.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+
+    public required string Name { get; set; }
+
+    [DataType(DataType.Currency)]
+    public double Price { get; set; }
+}
