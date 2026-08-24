2a576ff1764fdd097a049ed30965114c526ec2e4
Joe-Ortiz <7156063+Joe-Ortiz@users.noreply.github.com>
2026-08-24 14:37:37 -0700

    Remove Create/Edit/Details actions and views from Products
    Removed standard MVC Create, Edit, and Details actions from ProductsController and deleted their corresponding Razor views. The controller now only supports Index and GetProduct actions, indicating a move toward an API-driven or JavaScript-based CRUD workflow.

--- JSCRUD/Controllers/ProductsController.cs
+++ JSCRUD/Controllers/ProductsController.cs
@@ -19,97 +19,6 @@ public class ProductsController : Controller
         return View(await _context.Product.ToListAsync());
     }
 
-    // GET: PRODUCTS/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var product = await _context.Product
-            .FirstOrDefaultAsync(m => m.ProductId == id);
-        if (product == null)
-        {
-            return NotFound();
-        }
-
-        return View(product);
-    }
-
-    // GET: PRODUCTS/Create
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: PRODUCTS/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("ProductId,Name,Price")] Product product)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Add(product);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(product);
-    }
-
-    // GET: PRODUCTS/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var product = await _context.Product.FindAsync(id);
-        if (product == null)
-        {
-            return NotFound();
-        }
-        return View(product);
-    }
-
-    // POST: PRODUCTS/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int? id, [Bind("ProductId,Name,Price")] Product product)
-    {
-        if (id != product.ProductId)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(product);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(product.ProductId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(product);
-    }
-
     // GET: PRODUCTS/GetProduct/5
     [HttpGet]
     public async Task<IActionResult> GetProduct(int? id)
@@ -206,8 +115,4 @@ public class ProductsController : Controller
         };
     }
 
-    private bool ProductExists(int? productid)
-    {
-        return _context.Product.Any(e => e.ProductId == productid);
     }
-}
